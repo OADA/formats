3963425189661a9d722e44915b84d7e6f37f41b7
Alex Layton <alex@layton.in>
2020-04-09 00:20:21 -0400

    Fiddle with what to export

--- packages/oada-types/index.ts
+++ packages/oada-types/index.ts
@@ -1,6 +1,6 @@
-import formats from '@oada/formats'
+export { formats, Schema } from '@oada/formats'
 
-export { formats }
+export default formats
 
 export type TypeCheck<T> = (val: any) => val is T
 export type TypeAssert<T> = (val: any) => asserts val is T
