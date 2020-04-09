57f5e9fe61b77d1a20f23168846f6067fc18c89c
Alex Layton <alex@layton.in>
2020-04-09 00:19:32 -0400

    Fiddle with what to export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-//import { JSONSchema8 as Schema } from 'jsonschema8'
+export { JSONSchema8 as Schema } from 'jsonschema8'
 
 import schemas from './schemas'
 
-import { ajv } from './ajv'
+export { ajv as formats } from './ajv'
 
 // TODO: What the heck should I export??
-export default ajv
+export default formats
 
 export { OADAFormats } from './ajv'
 
