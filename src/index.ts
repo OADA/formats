280fd9234dfb4bd7029f03bb93dc28a6e0b3618d
Alex Layton <alex@layton.in>
2020-04-09 00:45:47 -0400

    Try to clean up build/deps stuff

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ export { JSONSchema8 as Schema } from 'jsonschema8'
 
 import schemas from './schemas'
 
-export { ajv as formats } from './ajv'
+import { ajv as formats } from './ajv'
 
 // TODO: What the heck should I export??
+export { schemas, formats }
 export default formats
 
 export { OADAFormats } from './ajv'
 
-export { schemas }
