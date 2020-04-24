c41b1e1e2c2fb8088736f015c375628ebf4d2b1f
Alex Layton <alex@layton.in>
2020-04-24 13:43:13 -0400

    Add basic tests for compiled types
    Failing tests are because of OADA/formats#3

---
+++ packages/oada-types/types.spec.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai'
+
+import { schemas } from '@oada/formats'
+
+import {TypeAssert, TypeCheck} from './'
+
+type TypeModule<T = unknown> = {
+  is: TypeCheck<T>
+  assert: TypeAssert<T>
+}
+describe('OADA Types', () => {
+  for (const { key, schema } of schemas()) {
+    const type = key.replace(/^\//, './').replace(/\.schema\.json$/, '')
+
+    describe(key, () => {
+      let typeModule: TypeModule
+      before('Load type module', async () => {
+        typeModule = await import(type)
+      })
+
+      describe('Examples', () => {
+        for(const i in schema.examples ?? []) {
+          const example = schema.examples?.[i]
+
+          it(`should check true for example ${i}`, () => {
+            expect(typeModule.is(example)).to.be.ok
+          })
+
+          it(`should assert example ${i}`, async () => {
+            try {
+              typeModule.assert(example)
+            } catch (err) {
+              expect(err).to.not.be
+            }
+          })
+        }
+      })
+    })
+  }
+})
