dd0226db4102070154abdc989278fec3faffe42a
Tomohiro Arakawa <tarakawa@purdue.edu>
2020-05-04 13:34:15 -0400

    support array-based change document

--- src/schemas/oada/change.schema.ts
+++ src/schemas/oada/change.schema.ts
@@ -8,15 +8,70 @@ const schema: Schema = {
       enum: ['merge', 'delete']
     }
   },
-  type: 'object',
-  required: ['type', 'body'],
-  properties: {
-    type: { $ref: '#/definitions/type' },
-    body: {
-      $comment: 'Should probably narrow this schema down',
-      description: 'The contents of what was changed.'
+  oneOf: [
+    {
+      type: 'object',
+      description: 'Tree-based change representation (OADA v1)',
+      required: ['type', 'body'],
+      properties: {
+        type: { $ref: '#/definitions/type' },
+        body: {
+          $comment: 'Should probably narrow this schema down',
+          description: 'The contents of what was changed.'
+        }
+      }
+    },
+    {
+      type: 'array',
+      description: 'Array-based change representation (OADA v2)',
+      items: {
+        type: 'object',
+        required: ['type', 'body', 'path', 'resource_id'],
+        properties: {
+          type: { $ref: '#/definitions/type' },
+          body: {
+            $comment: 'Should probably narrow this schema down',
+            description: 'The contents of what was changed.'
+          },
+          path: { $ref: '../oada.schema.json#/definitions/path' },
+          resource_id: { $ref: '../oada.schema.json#/definitions/_id' }
+        }
+      }
     }
-  }
+  ],
+  examples: [
+    // OADA v1
+    {
+      body: {
+        test: 'abc',
+        _rev: 1
+      },
+      type: 'merge'
+    },
+    // OADA v2
+    [
+      {
+        resource_id: 'resources/default:resources_bookmarks_123',
+        path: '',
+        body: {
+          rocks: {
+            _rev: 1
+          },
+          _rev: 1
+        },
+        type: 'merge'
+      },
+      {
+        resource_id: 'resources/default:resources_rocks_123',
+        path: '/rocks',
+        body: {
+          test: 'abc',
+          _rev: 1
+        },
+        type: 'merge'
+      }
+    ]
+  ]
 }
 
 export default schema
