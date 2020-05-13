3db3ca6328a6200995ad6c612c98c661a40cb0ea
Alex Layton <alex@layton.in>
2020-05-12 23:32:56 -0400

    Add job type of ainz rule
    old type is now called reindex

--- src/schemas/oada/ainz/rule.schema.ts
+++ src/schemas/oada/ainz/rule.schema.ts
@@ -5,31 +5,77 @@ const schema: Schema = {
   description: 'The format for ainz orchestation rules',
   type: 'object',
   properties: {
+    type: {
+      description: 'A field for discriminating the different types of rules'
+    },
     list: {
       description: 'A path to an OADA list to watch for changes',
-      $ref: '../../oada.schema.json#/definitions/path'
-    },
-    destination: {
-      description: 'A path to an OADA list to which to add maching items',
       $ref: '../../oada.schema.json#/definitions/path'
     },
     schema: {
       description: 'A JSON Schema to which to compare new items in list',
       $ref: 'https://json-schema.org/draft/2019-09/schema#'
-    },
-    meta: {
-      description: "And object to PUT on matching items's _meta",
-      type: 'object'
     }
   },
-  required: ['list', 'schema'],
+  oneOf: [
+    {
+      description: 'A rule which "moves" (re-indexes) a resource',
+      type: 'object',
+      properties: {
+        type: { enum: ['reindex'] },
+        destination: {
+          description: 'A path to an OADA list to which to add maching items',
+          $ref: '../../oada.schema.json#/definitions/path'
+        },
+        meta: {
+          description: "And object to PUT on matching items's _meta",
+          type: 'object'
+        }
+      }
+    },
+    {
+      description: 'A rule which creates an OADA job when a resource matches',
+      type: 'object',
+      properties: {
+        type: { enum: ['job'] },
+        pointer: {
+          description:
+            'A JSON pointer of where in the job config to create a link to the resource',
+          type: 'string',
+          format: 'json-pointer'
+        },
+        job: {
+          description:
+            'The job to create. A link to the matching resource will be added to the job.',
+          $ref: '../service/job.schema.json#'
+        }
+      }
+    }
+  ],
+  required: ['type', 'list', 'schema'],
   examples: [
     {
+      type: 'reindex',
       list: '/bookmarks/stuff',
       schema: {
         const: { foo: 'bar' }
       },
       destination: '/bookmarks/foo'
+    },
+    {
+      type: 'job',
+      list: '/bookmarks/stuff',
+      schema: {
+        const: { foo: 'bar' }
+      },
+      pointer: '/resource',
+      job: {
+        service: 'coolservice',
+        type: 'cool',
+        config: {
+          foo: 'bar'
+        }
+      }
     }
   ]
 }
